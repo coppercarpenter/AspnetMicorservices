10ef014d207bcedb77ed71f38e5b8eb5a1c25cd2
sheheryar sajid <sheheryar.sajid921@gmail.com>
2022-04-03 20:01:18 +0500

    Mongo Connection

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,27 @@
+﻿using Catalog.API.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+
+    internal class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration config)
+        {
+            var connectionString = config.GetValue<string>("DatabaseSettings:ConnectionString");
+            var databaseName = config.GetValue<string>("DatabaseSettings:DatabaseName");
+            var collectionName = config.GetValue<string>("DatabaseSettings:CollectionName");
+
+            // Used to create connection with MongoDb
+            var client = new MongoClient(connectionString);
+
+            //Get or create MongoDb 
+            var database = client.GetDatabase(databaseName);
+
+            Products = database.GetCollection<Product>(collectionName);
+            CatalogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.API.Entities
+{
+    public class Product
+    {
+        [BsonId]//MongoDb Id
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")] //Used to change the name of property inside of MongoDb
+        public string Name { get; set; }
+
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public string Summary { get; set; }
+        public string ImageFile { get; set; }
+        public decimal Price { get; set; }
+    }
+}
